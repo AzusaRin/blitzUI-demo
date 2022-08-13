c8dff169a0f628352d0c195256d92d4c107a6f21
Azusa <qq745847188@gmail.com>
2022-08-13 09:23:51 +0800

    加入parcel，将Button组件变为单文件vue组件

---
+++ src/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="bl-button">默认按钮</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.bl-button{
+  color: var(--button-color);
+  font-size: var(--font-size);
+  line-height: var(--button-line-height);
+  background-color: var(--button-bg);
+  padding: var(--button-padding);
+  border: 1px solid var(--button-border-color);
+  border-radius: var(--button-border-radius);
+  text-align: center;
+  cursor: pointer;
+  transition: .1s;
+
+
+
+  &:hover{
+    color: var(--button-color-hover);
+    border-color: var(--button-border-color-hover);
+    background-color: var(--button-bg-hover);
+  }
+  &:active{
+    color: var(--button-color-border-active);
+    border-color: var(--button-color-border-active);
+  }
+}
+
+</style>
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from "vue";
+import Button from "./Button";
+
+Vue.component('bl-button',Button)
+
+new Vue({
+    el:'#app'
+})
