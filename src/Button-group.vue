f825419bff2edb56dc5ea6a7f9ea0e66ae464e69
Azusa <qq745847188@gmail.com>
2022-08-13 15:04:25 +0800

    完成button group

---
+++ src/Button-group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bl-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script></script>
+<style lang="scss">
+.bl-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  > .bl-button {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:first-child {
+      border-top-left-radius: var(--button-border-radius);
+      border-bottom-left-radius: var(--button-border-radius);
+    }
+
+    &:last-child {
+      border-top-right-radius: var(--button-border-radius);
+      border-bottom-right-radius: var(--button-border-radius);
+    }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Button from "./Button";
 import Icon from "./Icon";
+import ButtonGroup from "./Button-group"
 
 Vue.component('bl-button',Button)
 Vue.component('bl-icon',Icon)
+Vue.component('bl-button-group',ButtonGroup)
 new Vue({
     el:'#app',
 
