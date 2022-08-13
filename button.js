ee0eca602180ae30295140e47fb8747d74babc1b
Azusa <qq745847188@gmail.com>
2022-08-13 11:10:00 +0800

    引入icon，iconPosition两个props。设置iconPosition验证器

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('bl-button',{
-template:`
-<button class="bl-button">默认按钮</button>
-`
-})
--- src/Button.vue
+++ src/Button.vue
@@ -1,11 +1,29 @@
 <template>
-  <button class="bl-button">默认按钮</button>
+  <button class="bl-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#icon-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return !(value !== 'left' && value !== 'right');
+      }
+    }
+  }
+}
 </script>
 <style lang="scss">
-.bl-button{
+.bl-button {
   color: var(--button-color);
   font-size: var(--font-size);
   line-height: var(--button-line-height);
@@ -16,15 +34,45 @@ export default {}
   text-align: center;
   cursor: pointer;
   transition: .1s;
-  &:hover{
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+
+  &:hover {
     color: var(--button-color-hover);
     border-color: var(--button-border-color-hover);
     background-color: var(--button-bg-hover);
+
   }
-  &:active{
+
+  &:active {
     color: var(--button-color-border-active);
     border-color: var(--button-color-border-active);
   }
+
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+
+  > .content {
+    order: 2;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: 0.3em;
+      margin-right: 0;
+    }
+
+    > .content {
+      order: 1;
+    }
+
+  }
 }
+
 
 </style>
