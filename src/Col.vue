2d5019e7cc8439aa87680885e77a8e706150cfd5
Azusa <qq745847188@gmail.com>
2022-08-17 15:13:23 +0800

    栅格样式，row和col雏形完成

---
+++ src/Col.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'BlitzCol',
+  props: {
+    span: {
+      type: [String, Number]
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.col {
+  height: 60px;
+  width: 100%;
+
+  &:nth-child(odd) {
+    background: rgb(140, 197, 255);
+  }
+
+  &:nth-child(even) {
+    background: #409EFF;
+  }
+
+  $class: col-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n/24)*100%;
+    }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./Button";
 import Icon from "./Icon";
 import ButtonGroup from "./Button-group"
 import Input from "./Input";
+import Row from "./Row";
+import Col from "./Col";
 
 Vue.component('bl-button',Button)
 Vue.component('bl-icon',Icon)
 Vue.component('bl-button-group',ButtonGroup)
 Vue.component('bl-input',Input)
+Vue.component('bl-row',Row)
+Vue.component('bl-col',Col)
 new Vue({
     el:'#app',
 
