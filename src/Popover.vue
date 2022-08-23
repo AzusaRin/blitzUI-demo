45eb29905c0dd48e152ab493bb5f443c3be22bde
Azusa <qq745847188@gmail.com>
2022-08-23 19:17:15 +0800

    实现最基本的popover功能

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,33 +1,45 @@
 <template>
-<div class="popover" @click="onClick">
-  <div class="content-wrapper" v-if="visible">
-  <slot name="content" ></slot>
+  <div class="popover" @click.stop="onClick">
+    <div class="content-wrapper" v-if="visible" @click.stop>
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
   </div>
-  <slot></slot>
-</div>
 </template>
 
 <script>
 export default {
-  name:'BlitzPopover',
-  data(){
-    return{
-      visible:false
+  name: 'BlitzPopover',
+  data() {
+    return {
+      visible: false
     }
   },
-  methods:{
-    onClick(){
-     this.visible = !this.visible
+  methods: {
+    onClick() {
+      this.visible = !this.visible
+      if (this.visible === true) {
+        this.$nextTick(() => {
+          let outerClick = () => {
+            this.visible = false
+            document.removeEventListener('click', outerClick)
+
+          }
+          document.addEventListener('click', outerClick)
+        })
+
+      }
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.popover{
+.popover {
   display: inline-block;
   position: relative;
-  .content-wrapper{
+
+  .content-wrapper {
     position: absolute;
     bottom: 100%;
     left: 0;
