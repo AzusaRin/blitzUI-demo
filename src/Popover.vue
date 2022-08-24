c4a55a07300ef7441b9444ef1e7945e53eef309b
Azusa <qq745847188@gmail.com>
2022-08-24 15:01:36 +0800

    解决popover有overflow hidden会被遮盖的问题

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="onClick">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div class="content-wrapper"   ref="contentWrapper"  v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
+    <span ref="switchWrapper">
     <slot></slot>
+      </span>
   </div>
 </template>
 
@@ -20,6 +22,10 @@ export default {
       this.visible = !this.visible
       if (this.visible === true) {
         this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper)
+        let {top,left}  =  this.$refs.switchWrapper.getBoundingClientRect()
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
           let outerClick = () => {
             this.visible = false
             document.removeEventListener('click', outerClick)
@@ -30,6 +36,9 @@ export default {
 
       }
     }
+  },
+  mounted() {
+
   }
 }
 </script>
@@ -38,11 +47,9 @@ export default {
 .popover {
   display: inline-block;
   position: relative;
-
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-  }
+}
+.content-wrapper {
+  position: absolute;
+  transform: translateY(-100%);
 }
 </style>
